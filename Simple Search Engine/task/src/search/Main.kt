11a7ce787622c1cc7c8ed3b45de0405eaa38dd64
Nikita Alexandrov <nikitosiks338@gmail.com>
2021-10-26 14:17:13 +0300

    Stage 1 passed, pure and safe IO made

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -1,5 +1,31 @@
 package search
 
+data class IO<A>(private val a: () -> A) {
+    fun unsafeRun(): A = a()
+
+    fun <B> withoutIO(f: (A) -> B): IO<B> =
+        IO { f(this.unsafeRun()) }
+
+    fun <B> continueIO(f: (A) -> IO<B>): IO<B> =
+        IO { f(this.unsafeRun()).unsafeRun() }
+}
+
+fun stdin(): IO<String> = IO { readLine().orEmpty() }
+fun stdout(msg: String): IO<Unit> = IO { println(msg) }
+
+fun search(where: String, what: String): Int? =
+    where.split(" ").withIndex().find { it.value == what }?.index?.plus(1)
+
+
 fun main() {
-    println("Hello, World!")
+    stdin().continueIO { whereSearch ->
+        stdin().continueIO { whatSearch ->
+            search(whereSearch, whatSearch).let { idx ->
+                when (idx) {
+                    null -> stdout("Not Found")
+                    else -> stdout("$idx")
+                }
+            }
+        }
+    }.unsafeRun()
 }
