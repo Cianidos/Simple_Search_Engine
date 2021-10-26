00c205790adbbec4f0bbf26a2a86b1f589624698
Nikita Alexandrov <nikitosiks338@gmail.com>
2021-10-26 21:00:04 +0300

    Passed 2 stage, very dirt

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -1,31 +1,64 @@
 package search
 
-data class IO<A>(private val a: () -> A) {
-    fun unsafeRun(): A = a()
+typealias Person = List<String>
 
-    fun <B> withoutIO(f: (A) -> B): IO<B> =
-        IO { f(this.unsafeRun()) }
+typealias Persons = List<Person>
 
-    fun <B> continueIO(f: (A) -> IO<B>): IO<B> =
-        IO { f(this.unsafeRun()).unsafeRun() }
+fun filterByWord(data: Persons, word: String) =
+    data.filter { containsWord(it, word) }
+
+fun containsWord(person: Person, word: String) =
+    person.map { it.lowercase() }.any { s -> s.contains(word.lowercase()) }
+
+fun readPersons(n: Int) =
+    (1..n).map { readLine().orEmpty().split(" ") }
+
+fun readPersonsCount() =
+    readLine().orEmpty().toInt()
+
+fun readRequestsCount() =
+    readLine().orEmpty().toInt()
+
+fun readRequest(): String {
+    println("Enter data to search people")
+    return readLine().orEmpty()
 }
 
-fun stdin(): IO<String> = IO { readLine().orEmpty() }
-fun stdout(msg: String): IO<Unit> = IO { println(msg) }
+fun println(person: Person) {
+    println(person.joinToString(" "))
+}
 
-fun search(where: String, what: String): Int? =
-    where.split(" ").withIndex().find { it.value == what }?.index?.plus(1)
-
+fun processFiltered(filteredData: Persons) {
+    when {
+        filteredData.isEmpty() -> {
+            println("No matching people found.")
+        }
+        else -> {
+            println()
+            println("People found:")
+            filteredData.forEach(::println)
+        }
+    }
+}
 
 fun main() {
-    stdin().continueIO { whereSearch ->
-        stdin().continueIO { whatSearch ->
-            search(whereSearch, whatSearch).let { idx ->
-                when (idx) {
-                    null -> stdout("Not Found")
-                    else -> stdout("$idx")
-                }
-            }
-        }
-    }.unsafeRun()
+    println("Enter the number of people:")
+    val data = readPersons(readPersonsCount())
+    println()
+    println("Enter the number of queries:")
+    val n = readRequestsCount()
+    println()
+
+    repeat(n) {
+        processFiltered(filterByWord(data, readRequest()))
+        println()
+    }
 }
+/*
+Dwight Joseph djo@gmail.com
+Rene Webb webb@gmail.com
+Katie Jacobs
+Erick Harrington harrington@gmail.com
+Myrtle Medina
+Erick Burgess
+ */
