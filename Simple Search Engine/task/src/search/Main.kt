618fa88d21c9e074fdb56fb80009e9a696d7d5ca
Nikita Alexandrov <nikitosiks338@gmail.com>
2021-10-28 17:35:48 +0300

    IO combinators done

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -23,15 +23,39 @@ fun processFiltered(filteredData: Persons) = when {
     )
 }
 
-data class IO<T>(val effect: () -> T) : () -> T by effect
+data class IO<T> private constructor(private val effect: () -> T) {
+    companion object {
+        operator fun <T> invoke(effect: IOContext.() -> T) =
+            IO { IOContext().effect() }
 
-fun stdin() = IO { readLine().orEmpty() }
+        operator fun <T> invoke(io: IO<T>) = io
+    }
+
+    private operator fun invoke() = effect()
+
+    infix fun <U> map(f: IOContext.(T) -> U) = IO { IOContext().f(this()) }
+    infix fun <U> and(io: IO<U>) = IO { this(); io() }
+    fun <U> mapT(f: T.(IOContext) -> U) = IO { this().f(IOContext()) }
+
+    infix fun <U> flatMap(f: IOContext.(T) -> IO<U>) =
+        IO { IOContext().f(this())() }
+
+    class IOContext {
+        fun <U> use(io: IO<U>): U = io()
+    }
+
+    fun unsafeRun() = effect()
+}
+
+
+val stdin = IO { readLine().orEmpty() }
 
 fun readPersons(n: Int): IO<List<List<String>>> =
-    IO { (1..n).map { stdin()().split(" ") } }
-fun readPersonsCount(): IO<Int> = IO { stdin()().toInt() }
-fun readRequestsCount(): IO<Int> = IO { stdin()().toInt() }
-fun readRequest() = stdin()
+    IO { (1..n).map { use(stdin.mapT { split(" ") }) } }
+
+val readPersonsCount: IO<Int> = stdin.mapT { toInt() }
+val readRequestsCount: IO<Int> = stdin.mapT { toInt() }
+val readRequest = stdin
 
 
 fun stdout(msg: String = "") = IO { println(msg) }
@@ -40,24 +64,22 @@ val enterNumberOfPeople = stdout("Enter the number of people:")
 val enterNumberOfQueries = stdout("\nEnter the number of queries:")
 val endLine = stdout()
 val enterDataToSearchPeople = stdout("Enter data to search people")
-val personsCount = readPersonsCount()
-val dataReading = readPersons(personsCount())
-val printProcessedData = { data: Persons, request: String ->
+val personsCount = readPersonsCount
+val dataReading = personsCount.map { use(readPersons(it)) }
+
+fun printProcessedData(data: Persons, request: String) =
     stdout(processFiltered(filterByWord(data, request)))
-}
 
-// TODO convert sequence of calls to function composition
 fun main() {
-    enterNumberOfPeople()
-    val data: List<List<String>> = dataReading()
-    enterNumberOfQueries()
-    val n = readRequestsCount()()
-    endLine()
-
-    repeat(n) {
-        enterDataToSearchPeople()
-        val request = readRequest()()
-        printProcessedData(data, request)()
-        endLine()
-    }
+    (enterNumberOfPeople and dataReading flatMap { data ->
+        enterNumberOfQueries and readRequestsCount flatMap { n ->
+            endLine map {
+                repeat(n) {
+                    use(enterDataToSearchPeople and readRequest.flatMap { request ->
+                        printProcessedData(data, request) and endLine
+                    })
+                }
+            }
+        }
+    }).unsafeRun()
 }
