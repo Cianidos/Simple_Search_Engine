ea7738cfe019ead348b427da6b1cf8acb7151d68
Nikita Alexandrov <nikitosiks338@gmail.com>
2021-10-28 17:54:08 +0300

    context and combinators design improved

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -24,18 +24,17 @@ fun processFiltered(filteredData: Persons) = when {
 }
 
 data class IO<T>(private val effect: IOContext.() -> T) {
-
     private operator fun invoke() = IOContext().effect()
 
     infix fun <U> map(f: IOContext.(T) -> U) = IO { IOContext().f(this@IO()) }
-    infix fun <U> and(io: IO<U>) = IO { this@IO(); io() }
+    operator fun <U> times(io: IO<U>) = IO { this@IO(); io() }
     fun <U> mapT(f: T.(IOContext) -> U) = IO { this@IO().f(IOContext()) }
 
     infix fun <U> flatMap(f: IOContext.(T) -> IO<U>) =
         IO { IOContext().f(this@IO())() }
 
     class IOContext {
-        fun <U> use(io: IO<U>): U = io()
+        operator fun <T> IO<T>.unaryPlus() = this()
     }
 
     fun unsafeRun() = this()
@@ -45,7 +44,7 @@ data class IO<T>(private val effect: IOContext.() -> T) {
 val stdin = IO { readLine().orEmpty() }
 
 fun readPersons(n: Int): IO<List<List<String>>> =
-    IO { (1..n).map { use(stdin.mapT { split(" ") }) } }
+    IO { (1..n).map { +stdin.mapT { split(" ") } } }
 
 val readPersonsCount: IO<Int> = stdin.mapT { toInt() }
 val readRequestsCount: IO<Int> = stdin.mapT { toInt() }
@@ -53,24 +52,24 @@ val readRequest = stdin
 
 
 fun stdout(msg: String = "") = IO { println(msg) }
+val endLine = stdout()
 
 val enterNumberOfPeople = stdout("Enter the number of people:")
 val enterNumberOfQueries = stdout("\nEnter the number of queries:")
-val endLine = stdout()
 val enterDataToSearchPeople = stdout("Enter data to search people")
 val personsCount = readPersonsCount
-val dataReading = personsCount.map { use(readPersons(it)) }
+val dataReading = personsCount.map { +readPersons(it) }
 
 fun printProcessedData(data: Persons, request: String) =
     stdout(processFiltered(filterByWord(data, request)))
 
 fun main() {
-    (enterNumberOfPeople and dataReading flatMap { data ->
-        enterNumberOfQueries and readRequestsCount flatMap { n ->
+    (enterNumberOfPeople * dataReading flatMap { data ->
+        enterNumberOfQueries * readRequestsCount flatMap { n ->
             endLine map {
                 repeat(n) {
-                    use(enterDataToSearchPeople and readRequest.flatMap { request ->
-                        printProcessedData(data, request) and endLine
+                    +(enterDataToSearchPeople * readRequest flatMap { request ->
+                        printProcessedData(data, request) * endLine
                     })
                 }
             }
